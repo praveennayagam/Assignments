411930a728ac8290ef9d74c186f99135b301bf18
praveennayagam <141718695+praveennayagam@users.noreply.github.com>
2025-12-24 17:25:30 +0530

    Delete PGAccomodation directory

--- PGAccomodation/src/main/java/com/mphasis/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mphasis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mphasis.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {}
--- PGAccomodation/src/main/java/com/mphasis/repository/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mphasis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mphasis.model.City;
-
-public interface CityRepository extends JpaRepository<City, Long> {} 
